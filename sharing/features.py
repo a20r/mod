a9862b12dc1577b2f430ae0eccd44e7f48576a2c
wallarelvo <alex@wallar.me>
2015-11-17 17:33:22 -0500

    Extracting features now

--- sharing/features.py
+++ sharing/features.py
@@ -6,17 +6,43 @@ import numpy as np
 import sklearn.cluster as cluster
 
 
-features_names = ["pickup_time", "dropoff_time", "passenger_count",
-                  "pickup_station_lon", "pickup_station_lat",
-                  "dropoff_station_lon", "dropoff_station_lat"]
+feature_names = ["p_time", "d_time", "p_day",
+                 "passenger_count", "p_station_lon",
+                 "p_station_lat", "d_station_lon",
+                 "d_station_lat"]
+
+
+def percent_time(str_time):
+    """
+    Given a string representation of a date, this returns the percent of the
+    day that the time is (i.e. 0.5 would be 12 noon)
+    """
+    date_format = "%Y-%m-%d %H:%M:%S"
+    t = time.strptime(str_time, date_format)
+    secs = t.tm_hour * 60 * 60 + t.tm_min * 60 + t.tm_sec
+    return secs / (24 * 60 * 60.0), t.tm_wday
+
+
+def clean_dict(val_dict):
+    clean = dict()
+    for key in val_dict.keys():
+        k = key.strip()
+        try:
+            clean[k] = float(val_dict[key])
+        except:
+            clean[k] = val_dict[key]
+    return clean
 
 
 def find_stations(fn_in, **kwargs):
+    """
+    Uses k-means clustering to determine the taxi stations using the drop-off
+    and pick-up locations
+    """
     with io.open(fn_in, "rb") as fin:
         reader = csv.reader(fin)
         fl = sum(1 for _ in reader) - 1
-    with io.open(fn_in, "rb") as fin:
-        reader = csv.reader(fin)
+        fin.seek(0)
         kmeans = cluster.KMeans(**kwargs)
         points = np.zeros((2 * fl, 2))
         for i, row in enumerate(reader):
@@ -30,35 +56,35 @@ def find_stations(fn_in, **kwargs):
         return kmeans, points
 
 
-def percent_time(str_time):
-    date_format = "%Y-%m-%d %H:%M:%S"
-    t = time.strptime(str_time, date_format)
-    secs = t.tm_hour * 60 * 60 + t.tm_min * 60 + t.tm_sec
-    return secs / (24 * 60 * 60.0)
-
-
 def extract_features(fn_in, fn_out):
+    """
+    Extracts features from the in put file `fn_in` and writes the features
+    as a csv file to `fn_out`
+    """
     stations, points = find_stations(fn_in)
     with io.open(fn_in, "rb") as fin:
         with io.open(fn_out, "wb") as fout:
-            reader = csv.reader(fin)
-            writer = csv.writer(fout)
+            reader = csv.DictReader(fin)
+            writer = csv.DictWriter(fout, fieldnames=feature_names)
+            writer.writeheader()
             for i, row in enumerate(reader):
                 if i == 0:
-                    writer.writerow(features_names)
-                else:
-                    features = [None] * len(features_names)
-                    locs = np.array([[float(row[10]), float(row[11])],
-                                     [float(row[12]), float(row[13])]])
-                    sts = points[stations.predict(locs)]
-                    features[0] = percent_time(row[5])
-                    features[1] = percent_time(row[6])
-                    features[2] = row[7]
-                    features[3] = sts[0][0]
-                    features[4] = sts[0][1]
-                    features[5] = sts[1][0]
-                    features[6] = sts[1][1]
-                    writer.writerow(features)
+                    continue
+                row = clean_dict(row)
+                features = dict()
+                locs = np.array([
+                    [row["pickup_longitude"], row["pickup_latitude"]],
+                    [row["dropoff_longitude"], row["dropoff_longitude"]]])
+                sts = points[stations.predict(locs)]
+                features["passenger_count"] = row["passenger_count"]
+                features["p_day"] = percent_time(row["pickup_datetime"])[1]
+                features["p_time"] = percent_time(row["pickup_datetime"])[0]
+                features["d_time"] = percent_time(row["dropoff_datetime"])[0]
+                features["p_station_lon"] = sts[0][0]
+                features["p_station_lat"] = sts[0][1]
+                features["d_station_lon"] = sts[1][0]
+                features["d_station_lat"] = sts[1][1]
+                writer.writerow(features)
 
 
 if __name__ == "__main__":
