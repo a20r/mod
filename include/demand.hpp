255a81ee36c70ab60c99ac45118cea768dc25748
wallarelvo <alex@wallar.me>
2016-01-05 15:30:01 +0800

    Updated the coding style to make it more readable

--- include/demand.hpp
+++ include/demand.hpp
@@ -13,52 +13,60 @@
 
 using namespace std;
 
-namespace mod {
+namespace mod
+{
 
-    class GeoLocation {
+    class GeoLocation
+    {
         public:
             double lat, lon;
             GeoLocation() {};
             ~GeoLocation() {};
-            GeoLocation(double lat, double lon) : lat(lat), lon(lon) {};
+            GeoLocation(double lat, double lon) : lat(lat), lon(lon)
+            {
+            };
 
-            double distance(GeoLocation& gl) {
+            double distance(GeoLocation& gl)
+            {
                 double dlat2 = pow(gl.lat - lat, 2);
                 double dlon2 = pow(gl.lon - lon, 2);
                 return sqrt(dlat2 + dlon2);
             }
     };
 
-    class Time {
+    class Time
+    {
         public:
             int secs, day;
 
             Time() {};
             ~Time() {};
-            Time(int secs, int day) : secs(secs), day(day) {};
+            Time(int secs, int day) : secs(secs), day(day)
+            {
+            };
 
-            int get_interval() const {
+            int get_interval() const
+            {
                 return secs / (15 * 60);
             }
     };
 
-    class Demand {
+    class Demand
+    {
         public:
             int tau, day, pickup, dropoff;
 
             Demand() {};
-
-            Demand(int tau, int day, int pickup, int dropoff) {
-                this->tau = tau;
-                this->day = day;
-                this->pickup = pickup;
-                this->dropoff = dropoff;
+            Demand(int tau, int day, int pickup, int dropoff) :
+                tau(tau), day(day), pickup(pickup), dropoff(dropoff)
+            {
             }
     };
 
     class DemandHash {
         public:
-            size_t operator()(const Demand& demand) const {
+            size_t operator() (const Demand& demand) const
+            {
                 stringstream buffer;
                 buffer << demand.tau << " ";
                 buffer << demand.day << " ";
@@ -68,24 +76,24 @@ namespace mod {
             }
     };
 
-    inline bool operator==(Demand const& lhs, Demand const& rhs) {
+    inline bool operator== (Demand const& lhs, Demand const& rhs) {
         return lhs.tau == rhs.tau and lhs.day == rhs.day and
             lhs.pickup == rhs.pickup and lhs.dropoff == rhs.dropoff;
     };
 
-    inline bool operator<=(Time const& lhs, Demand const& rhs) {
+    inline bool operator<= (Time const& lhs, Demand const& rhs) {
         return lhs.day <= rhs.day and lhs.get_interval() <= rhs.tau;
     };
 
-    inline bool operator>(Time const& lhs, Demand const& rhs) {
+    inline bool operator> (Time const& lhs, Demand const& rhs) {
         return lhs.day >= rhs.day and lhs.get_interval() > rhs.tau;
     };
 
-    inline bool operator>=(Time const& lhs, Demand const& rhs) {
+    inline bool operator>= (Time const& lhs, Demand const& rhs) {
         return lhs.day >= rhs.day and lhs.get_interval() >= rhs.tau;
     };
 
-    inline ostream& operator<<(ostream& os, const Demand& demand) {
+    inline ostream& operator<< (ostream& os, const Demand& demand) {
         os << "Demand(";
         os << demand.tau << ", ";
         os << demand.day << ", ";
@@ -95,8 +103,8 @@ namespace mod {
         return os;
     };
 
-    class DemandLookup {
-
+    class DemandLookup
+    {
         private:
             unordered_map<Demand, double, DemandHash> demands;
             vector<Demand> demand_vec;
@@ -108,12 +116,14 @@ namespace mod {
             DemandLookup() {};
             ~DemandLookup() {};
 
-            DemandLookup(string fn_stations, string fn_probs) {
+            DemandLookup(string fn_stations, string fn_probs)
+            {
                 load_probs(fn_probs);
                 load_stations(fn_stations);
             }
 
-            void load_probs(string fn_probs) {
+            void load_probs(string fn_probs)
+            {
                 int tau, day, pickup, dropoff;
                 double prob;
                 ifstream data(fn_probs);
@@ -121,12 +131,15 @@ namespace mod {
                 getline(data, line);
 
                 double lp = 0;
-                while(getline(data, line)) {
+                while(getline(data, line))
+                {
                     stringstream lineStream(line);
                     string cell;
                     int counter = 0;
-                    while(std::getline(lineStream, cell, ',')) {
-                        switch (counter++) {
+                    while(std::getline(lineStream, cell, ','))
+                    {
+                        switch (counter++)
+                        {
                             case 0: tau = stoi(cell);
                             case 1: day = stoi(cell);
                             case 2: pickup = stoi(cell);
@@ -144,18 +157,22 @@ namespace mod {
                 }
             }
 
-            void load_stations(string fn_stations) {
+            void load_stations(string fn_stations)
+            {
                 double lat, lon;
                 ifstream data(fn_stations);
                 string line;
                 getline(data, line);
 
-                while(getline(data, line)) {
+                while(getline(data, line))
+                {
                     stringstream lineStream(line);
                     string cell;
                     int counter = 0;
-                    while(std::getline(lineStream, cell, ',')) {
-                        switch (counter++) {
+                    while(std::getline(lineStream, cell, ','))
+                    {
+                        switch (counter++)
+                        {
                             case 1: lat = stof(cell);
                             case 2: lon = stof(cell);
                             default: break;
@@ -165,16 +182,22 @@ namespace mod {
                 }
             }
 
-            int get_station(GeoLocation gl) {
+            int get_station(GeoLocation gl)
+            {
                 double min_dist;
                 int min_id;
-                for (size_t i = 0; i < stations.size(); i++) {
+                for (size_t i = 0; i < stations.size(); i++)
+                {
                     double dist = stations[i].distance(gl);
-                    if (i == 0) {
+                    if (i == 0)
+                    {
                         min_id = 0;
                         min_dist = dist;
-                    } else {
-                        if (dist < min_dist) {
+                    }
+                    else
+                    {
+                        if (dist < min_dist)
+                        {
                             min_id = i;
                             min_dist = dist;
                         }
@@ -183,37 +206,48 @@ namespace mod {
                 return min_id;
             }
 
-            GeoLocation get_station(int id) {
+            GeoLocation get_station(int id)
+            {
                 return stations[id];
             }
 
 
-            double query_demand(Demand dem) {
-                if (demands.count(dem) > 0) {
+            double query_demand(Demand dem)
+            {
+                if (demands.count(dem) > 0)
+                {
                     return demands[dem];
-                } else {
+                }
+                else
+                {
                     return 0;
                 }
             }
 
-            double query_demand(int tau, int day, int pickup, int dropoff) {
+            double query_demand(int tau, int day, int pickup, int dropoff)
+            {
                 return query_demand(Demand(tau, day, pickup, dropoff));
             }
 
             double query_demand(int secs, int day, GeoLocation pickup,
-                    GeoLocation dropoff) {
+                    GeoLocation dropoff)
+            {
                 int p_st = get_station(pickup);
                 int d_st = get_station(dropoff);
                 int tau = secs / (60 * 15);
                 return query_demand(tau, day, p_st, d_st);
             }
 
-            void sample(int num, vector<double>& csum, Demand dems[]) {
-                for (int i = 0; i < num; i++) {
+            void sample(int num, vector<double>& csum, Demand dems[])
+            {
+                for (int i = 0; i < num; i++)
+                {
                     double rnd = (double) rand() / (double) (RAND_MAX);
                     double r = csum[0] + (csum.back() - csum[0]) * rnd;
-                    for (size_t j = 0; j < csum.size(); j++) {
-                        if (r < csum[j]) {
+                    for (size_t j = 0; j < csum.size(); j++)
+                    {
+                        if (r < csum[j])
+                        {
                             dems[i] = demand_vec[j];
                             break;
                         }
@@ -221,14 +255,19 @@ namespace mod {
                 }
             }
 
-            void sample(int num, Time st, Time end, Demand dems[]) {
+            void sample(int num, Time st, Time end, Demand dems[])
+            {
                 // This only works if the probability file is sorted by time
                 vector<double> csum;
                 double lp = 0;
-                for (size_t i = 0; i < demand_vec.size(); i++) {
-                    if (end > demand_vec[i]) {
+                for (size_t i = 0; i < demand_vec.size(); i++)
+                {
+                    if (end > demand_vec[i])
+                    {
                         break;
-                    } else if (st >= demand_vec[i] and end <= demand_vec[i]) {
+                    }
+                    else if (st >= demand_vec[i] and end <= demand_vec[i])
+                    {
                         csum.push_back(lp + demands[demand_vec[i]]);
                         lp += demands[demand_vec[i]];
                     }
