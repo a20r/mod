b2dde3243a7294a7fd2a9e3b68a96b169a74b4d0
Alexander Wallar <alex@wallar.me>
2016-07-15 14:46:44 -0400

    Updating comp times table

--- scripts/tabler.py
+++ scripts/tabler.py
@@ -1,15 +1,12 @@
 
-# import json
+import pandas as pd
 from tabulate import tabulate
-# from load_metrics import get_ready_folders, FolderInfo
-# from progressbar import ProgressBar, ETA, Percentage, Bar
-# import common
 from plot_metrics import get_avg_dataframe, prettify
 
 
 NFS_PATH = "/home/wallar/nfs/data/data-sim/"
 # NFS_PATH = "/data/drl/mod_sim_data/data-sim/"
-OUTPUT_PATH = "/home/wallar/www/table.html"
+OUTPUT_PATH = "/home/wallar/www/{}.html"
 TEMPLATE_PATH = "sandbox/table_template.html"
 
 
@@ -20,6 +17,9 @@ ordered_fields = ["vehicles", "capacity", "waiting_time", "predictions",
                   "total_km_travelled", "km_travelled_per_car",
                   "empty_rebalancing", "empty_moving_to_pickup",
                   "empty_waiting", "not_empty", "active_taxis", "n_shared"]
+
+
+ct_fields = ["vehicles", "capacity", "waiting_time", "comp_time"]
 
 
 def avg_cols(df):
@@ -56,24 +56,35 @@ def find_permutation(control, alternate):
     return perm
 
 
-def create_table():
-    df = get_avg_dataframe()
+def create_table(df, ord_fields, tablefmt):
     datas = df.to_dict(orient="records")
     headers = datas[0].keys()
-    perm = find_permutation(ordered_fields, headers)
+    perm = find_permutation(ord_fields, headers)
     datas = map(lambda v: v.values(), datas)
     datas = rearrange(datas, perm)
     datas = sorted(datas, cmp=comparator)
-    ord_headers = map(prettify, ordered_fields)
-    return tabulate(datas, ord_headers, tablefmt="latex")
+    ord_headers = map(prettify, ord_fields)
+    return tabulate(datas, ord_headers, tablefmt=tablefmt)
 
 
 if __name__ == "__main__":
-    tab = create_table()
-    """
-    with open(OUTPUT_PATH, "w") as fout:
+    df = get_avg_dataframe()
+    tab = create_table(df, ordered_fields, "html")
+    with open(OUTPUT_PATH.format("table"), "w") as fout:
         with open(TEMPLATE_PATH) as fin:
             fout.write(fin.read() + "\n" + tab)
-    """
+    tabl = create_table(df, ordered_fields, "latex")
     with open("table.tex", "w") as fout:
         fout.write(tab)
+    ct_df = pd.read_csv("data/times.csv")
+    ct_df.drop("Unnamed: 0", axis=1, inplace=True)
+    ct_df = ct_df.groupby(["vehicles", "capacity", "waiting_time"]).mean()
+    ct_df.drop("day", axis=1, inplace=True)
+    ct_df.reset_index(inplace=True)
+    tab = create_table(ct_df, ct_fields, "latex")
+    with open("comp_times_table.tex", "w") as fout:
+        fout.write(tab)
+    tab = create_table(ct_df, ct_fields, "html")
+    with open(OUTPUT_PATH.format("comp_times"), "w") as fout:
+        with open(TEMPLATE_PATH) as fin:
+            fout.write(fin.read() + "\n" + tab)
