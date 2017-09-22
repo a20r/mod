450ba696b1474c62fb9dd085d504528be1ed605a
Alex Wallar <alex@wallar.me>
2017-09-22 22:53:08 +0200

    stupid travis

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ git clone https://github.com/rogersce/cnpy third_party/cnpy
 mkdir -p third_party/cnpy/build
 cd third_party/cnpy; git reset --hard b39d58d3640f77c043bfe92591afeafdd82e78db; cd -;
 cd third_party/cnpy/build; cmake ..; cd -;
-cd third_party/cnpy/build; make; sudo make install; cd -;
+cd third_party/cnpy/build; make; make install; cd -;
